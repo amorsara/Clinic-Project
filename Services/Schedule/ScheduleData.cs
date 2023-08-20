f424d16e0fc8f1189b6c01924bac336870fbb595
amorsara <samor2128@gmail.com>
2023-08-20 12:50:58 +0300

    sari 20-08-23

--- Services/Schedule/ScheduleData.cs
+++ Services/Schedule/ScheduleData.cs
@@ -56,7 +56,7 @@ namespace Services.Schedule
                 scheduleDto.phone1 = contact?.Phonenumber1;
                 scheduleDto.phonen2 = contact?.Phonenumber2;
                 scheduleDto.phone3 = contact?.Phonenumber3;
-                scheduleDto.detailsType = appointment.Area != null ? appointment.Area : (appointment.Duration != null ? appointment.Duration.ToString() : null);
+                scheduleDto.detailsType = appointment.Duration != null ? appointment.Duration.ToString() : (appointment.Area != null ? appointment.Area : null);
                 list.Add(scheduleDto);
             }
             return list;
@@ -85,4 +85,6 @@ namespace Services.Schedule
 
     }
 }
+
+// dd
 
